f7b7f1decf547226d8238a763f956761b5c81348
Rafael Bittencourt <almeidabittencourt@gmail.com>
2022-12-09 21:15:13 -0300

    Arquivo .csv adicionado no projeto, desempenho.py atualizado e bibliotecas adicionadas no requirements.txt

--- desempenho.py
+++ desempenho.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import seaborn as sns
 
 with st.expander('Cabeçalho'):
     st.title('Programa Diversidade Tech')
@@ -15,43 +19,181 @@ with st.expander('Cabeçalho'):
 with st.expander('Introdução'):
     st.title('Análise Exploratória')
     st.markdown("""
-    A seguir teremos uma análise exploratória dos dados referentes uma pesquisa sobre notas e algumas características de alunos do primeiro, segundo e terceiro ano de 2 escolas portuguesas.
+    A seguir teremos uma análise exploratória dos dados referentes uma pesquisa sobre 
     
     ### Dataset Columns
     """)
     st.markdown("""
-    1. school - Escola (binary: 'GP' - Gabriel Pereira ou 'MS' - Mousinho da Silveira)
-    2. sex - sexo (binary: 'F' - feminino ou 'M' - masculino)
-    3. age - idade (numeric: de 15 a 22)
-    4. address - tipo de endereço (binary: 'U' - urbano ou 'R' - rural)
-    5. famsize - tamanho da família (binary: 'LE3' - menor ou igual a 3 ou 'GT3' - maior que 3)
-    6. Pstatus - status dos pais (binary: 'T' - morando juntos ou 'A' - separados)
-    7. Medu - educação da mãe (numeric: 0 - nenhuma, 1 - educação primária (até quarta série), 2 - quinto ao nono ano, 3 - ensino médio ou 4 - ensino superior)
-    8. Fedu - educação do pai (numeric: 0 - nenhuma, 1 - educação primária (até quarta série), 2 - quinto ao nono ano, 3 - ensino médio ou 4 - ensino superior)
-    9. Mjob - trabalho da mãe (nominal: 'teacher', 'health' care related, civil 'services' (e.g. administrative or police), 'at_home' or 'other')
-    10. Fjob - trabalho do pai (nominal: 'teacher', 'health' care related, civil 'services' (e.g. administrative or police), 'at_home' or 'other')
-    11. reason - razão por escolher a escola (nominal: close to 'home', school 'reputation', 'course' preference or 'other')
-    12. guardian - student's guardian (nominal: 'mother', 'father' or 'other')
-    13. traveltime - home to school travel time (numeric: 1 - <15 min., 2 - 15 to 30 min., 3 - 30 min. to 1 hour, or 4 - >1 hour)
-    14. studytime - weekly study time (numeric: 1 - <2 hours, 2 - 2 to 5 hours, 3 - 5 to 10 hours, or 4 - >10 hours)
-    15. failures - number of past class failures (numeric: n if 1<=n<3, else 4)
-    16. schoolsup - extra educational support (binary: yes or no)
-    17. famsup - family educational support (binary: yes or no)
-    18. paid - extra paid classes within the course subject (Math or Portuguese) (binary: yes or no)
-    19. activities - extra-curricular activities (binary: yes or no)
-    20. nursery - attended nursery school (binary: yes or no)
-    21. higher - wants to take higher education (binary: yes or no)
-    22. internet - Internet access at home (binary: yes or no)
-    23. romantic - with a romantic relationship (binary: yes or no)
-    24. famrel - quality of family relationships (numeric: from 1 - very bad to 5 - excellent)
-    25. freetime - free time after school (numeric: from 1 - very low to 5 - very high)
-    26. goout - going out with friends (numeric: from 1 - very low to 5 - very high)
-    27. Dalc - workday alcohol consumption (numeric: from 1 - very low to 5 - very high)
-    28. Walc - weekend alcohol consumption (numeric: from 1 - very low to 5 - very high)
-    29. health - current health status (numeric: from 1 - very bad to 5 - very good)
-    30. absences - number of school absences (numeric: from 0 to 93)
-    these grades are related with the course subject, Math or Portuguese:
-    31. G1 - first period grade (numeric: from 0 to 20)
-    32. G2 - second period grade (numeric: from 0 to 20)
-    33. G3 - final grade (numeric: from 0 to 20, output target)
+    1. gender - Gênero (booleana: 'female' ou 'male')
+    2. race/ethnicity - raça/etnia (nominal: 5 grupos 'group A', 'group B' etc.)
+    3. parental level of education - nível educacional dos responsáveis (nominal: 'some college', '')
+    4.
     """)
+    
+# função que carrega o dataset
+def carregar_dataset(nome_dataset):
+    return pd.read_csv(nome_dataset)
+
+st.header('Dataset')
+nome_dataset = st.text_input('Qual o nome do dataset que deseja carregar?',
+                             value='desempenho')
+
+if nome_dataset:
+    df = carregar_dataset('StudentsPerformance.csv')
+
+# SIDEBAR
+## SIDEBAR - Filtro dos campos
+with st.sidebar:
+    st.title('Filtros')
+    cols_selected = \
+        st.multiselect('Filtre os campos que deseja analisar:',
+                       options=list(df.columns),
+                       default=list(df.columns))
+    frac_sample = \
+        st.slider('Defina o percentual do dataset para análise:', 
+                  min_value=1, max_value=100, value=50, step=1)
+
+df_selected = df[cols_selected].sample(frac=frac_sample/100)
+
+## CORPO - Info do dataset
+with st.expander('Dados do Dataset'):
+    st.header('Dados do Dataset')
+    st.subheader('Primeiros registros')
+    st.write(df_selected.head())
+    
+    st.subheader('Colunas')
+    for col in df_selected.columns:
+        st.text(f'- {col}')
+        
+    st.subheader('Dados Faltantes')
+    st.write(df_selected.isna().sum()[df_selected.isna().sum() > 0])
+
+    st.subheader('Estatísticas Descritivas')
+    st.write(df_selected.describe())
+
+## CORPO - Análise Univariada
+st.header('Análise Univariada')
+univar_campo =  \
+    st.selectbox('Selecione o campo que deseja avaliar a distribuição:',
+                 options=list(df_selected.select_dtypes(include=np.number)))
+
+
+st.plotly_chart(px.histogram(data_frame=df_selected, x=univar_campo))
+st.plotly_chart(px.box(data_frame=df_selected, y=univar_campo))
+
+## CORPO - Análise Bivariada
+st.header('Análise Bivariada')
+bivar_graf_option = \
+    st.radio('Escolha um tipo de gráfico:',
+             options=['dispersão', 'boxplot', 'pairplot'])
+
+### CORPO - Análise Bivariada - gráfico de dispersão
+if bivar_graf_option == 'dispersão':
+    campo_dispersao_1 =  \
+        st.selectbox('Selecione primeira variável numérica:',
+                     options=list(df_selected.select_dtypes(include=np.number)))
+        
+    campo_dispersao_2 =  \
+        st.selectbox('Selecione segunda variável numérica:',
+                     options=list(df_selected.select_dtypes(include=np.number)))
+        
+    st.plotly_chart(
+        px.scatter(data_frame=df_selected, 
+                   x=campo_dispersao_1, 
+                   y=campo_dispersao_2)
+    )
+
+### CORPO - Análise Bivariada - gráfico de boxplot       
+elif bivar_graf_option == 'boxplot':
+    campo_boxplot_num =  \
+        st.selectbox('Selecione uma variável numérica:',
+                     options=list(df_selected.select_dtypes(include=np.number)))
+        
+    campo_boxplot_cat =  \
+        st.selectbox('Selecione uma variável categórica:',
+                     options=list(df_selected.select_dtypes(exclude=np.number)))
+        
+    st.plotly_chart(
+        px.box(data_frame=df_selected, 
+                   x=campo_boxplot_cat, 
+                   y=campo_boxplot_num)
+    )
+
+### CORPO - Análise Bivariada - gráfico de pairplot  
+else:
+    pairplot = sns.pairplot(df_selected)
+    st.pyplot(pairplot)
+
+## CORPO - Análise Multivariada
+st.header('Análise Multivariada')
+multivar_graf_option = \
+    st.radio('Escolha um tipo de gráfico:',
+             key='multivar_graf_option',
+             options=['dispersão', 'boxplot', 'pairplot'])
+
+if multivar_graf_option == 'dispersão':
+    multivar_dispersao_num1 =  \
+        st.selectbox('Selecione primeira variável numérica:',
+                     key='multivar_dispersao_num1',
+                     options=list(df_selected.select_dtypes(include=np.number)))
+        
+    multivar_dispersao_num2 =  \
+        st.selectbox('Selecione segunda variável numérica:',
+                     key='multivar_dispersao_num2',
+                     options=list(df_selected.select_dtypes(include=np.number)))
+        
+    multivar_dispersao_seg = \
+        st.selectbox('Selecione uma variável categórica para segmentar:',
+                     key='multivar_dispersao_seg',
+                     options=list(df_selected.select_dtypes(exclude=np.number)))
+    
+    multivar_dispersao_check = \
+        st.checkbox('Adicionar linha de tendência')
+    
+    if multivar_dispersao_check:
+        st.plotly_chart(
+            px.scatter(data_frame=df_selected, 
+                    x=multivar_dispersao_num1, 
+                    y=multivar_dispersao_num2,
+                    color=multivar_dispersao_seg,
+                    trendline='ols')
+        )
+    else:
+        st.plotly_chart(
+            px.scatter(data_frame=df_selected, 
+                    x=multivar_dispersao_num1, 
+                    y=multivar_dispersao_num2,
+                    color=multivar_dispersao_seg)
+        )
+        
+elif multivar_graf_option == 'boxplot':
+    multivar_boxplot_num =  \
+        st.selectbox('Selecione uma variável numérica:',
+                     key='multivar_boxplot_num',
+                     options=list(df_selected.select_dtypes(include=np.number)))
+        
+    multivar_boxplot_cat =  \
+        st.selectbox('Selecione uma variável categórica:',
+                     key='multivar_boxplot_cat',
+                     options=list(df_selected.select_dtypes(exclude=np.number)))
+        
+    multivar_boxplot_seg =  \
+        st.selectbox('Selecione uma variável categórica para segmentar:',
+                     key='multivar_boxplot_seg',
+                     options=list(df_selected.select_dtypes(exclude=np.number)))
+        
+    st.plotly_chart(
+        px.box(data_frame=df_selected, 
+                   x=multivar_boxplot_cat, 
+                   y=multivar_boxplot_num,
+                   color=multivar_boxplot_seg)
+    )
+    
+else:
+    multivar_pairplot_seg =  \
+        st.selectbox('Selecione uma variável categórica:',
+                     key='multivar_pairplot_seg',
+                     options=list(df_selected.select_dtypes(exclude=np.number)))
+        
+    multivar_pairplot = sns.pairplot(df_selected, hue=multivar_pairplot_seg)
+    st.pyplot(multivar_pairplot)   
